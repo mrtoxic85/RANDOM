d06c2291bcd0541b8ad89a4dbfe8c370d56a9250
MEHEDI HASAN <107046336+H4X-GG@users.noreply.github.com>
2022-12-09 00:44:46 +0600

    Add files via upload

---
+++ run.py
@@ -0,0 +1,7 @@
+import os,platform
+os.system('git pull')
+print('\033[1;36m[•] \033[1;32mChecking Updates...')
+os.system('xdg-open https://facebook.com/groups/590005482506415/')
+import RANDOM
+DUMP.H4X()
+ 
