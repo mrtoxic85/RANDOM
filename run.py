2b9a050e14ddab8bed5d85f20a5af50e6d6f4bb2
MEHEDI HASAN <107046336+H4X-GG@users.noreply.github.com>
2022-12-26 22:09:14 +0600

    Add files via upload

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ os.system('git pull')
 print('\033[1;36m[•] \033[1;32mChecking Updates...')
 os.system('xdg-open https://facebook.com/groups/590005482506415/')
 try:ngr = requests.get("http://ip-api.com/json/").json();bas = ngr["country"]
-except:bas = "India"
+except:bas = "Bangladesh"
 if __name__ == "__main__":
-	if "India" == bas:
+	if "Bangladesh" == bas:
 		__import__("RANDOM").h4x()
 	else:exit("sorry this script is only for Bangladesi people")
