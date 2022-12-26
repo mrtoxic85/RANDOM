cc87f095dad99481bb5f390f4861735a11e2f10f
MEHEDI HASAN <107046336+H4X-GG@users.noreply.github.com>
2022-12-26 22:01:30 +0600

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,11 @@ import os,platform
 os.system('git pull')
 print('\033[1;36m[•] \033[1;32mChecking Updates...')
 os.system('xdg-open https://facebook.com/groups/590005482506415/')
-import RANDOM
-DUMP.H4X()
- 
+
+
+ try:ngr = requests.get("http://ip-api.com/json/").json();bas = ngr["country"]
+except:bas = "Bangladesh"
+if __name__ == "__main__":
+	if "Bangladesh" == bas:
+		__import__("importbahan").make()
+	else:exit("sorry this script is only for Bangladesi people")
