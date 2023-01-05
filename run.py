3f0b8cb9b2379937c028281c8b298761854a7911
MEHEDI HASAN <107046336+H4X-GG@users.noreply.github.com>
2023-01-05 11:12:02 +0600

    Add files via upload

---
+++ run.py
@@ -0,0 +1,14 @@
+import os,platform
+
+os.system('git pull')
+
+print('\033[1;36m[•] \033[1;32mChecking Updates...')
+
+os.system('xdg-open https://facebook.com/groups/590005482506415/')
+
+import rndm 
+
+rndm.main_apv()
+
+ 
+
