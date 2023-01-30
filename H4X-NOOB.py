01c796ee1665d0e4102f0fcc2fd091882162fb78
MEHEDI HASAN <107046336+H4X-GG@users.noreply.github.com>
2023-01-30 17:58:41 +0600

    Update H4X-NOOB.py

--- H4X-NOOB.py
+++ H4X-NOOB.py
@@ -6,7 +6,5 @@ print('\033[1;36m[•] \033[1;32mChecking Updates...')
 
 os.system('xdg-open https://facebook.com/groups/590005482506415/')
 
-os.system('clear')
-print('\n\t\033[1;32 H4X has left this community ')
- 
+import rndm64
 
